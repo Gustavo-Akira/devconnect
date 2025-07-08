a14d6fd6473306ad0d430c87e8cbd1fb619a8ec7
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-07-07 23:40:54 -0300

    feat: adding saveDevProfile endpoint with facade to aggregate usecases

--- src/main/java/br/com/gustavoakira/devconnect/adapters/inbound/controller/devprofile/DevProfileController.java
+++ src/main/java/br/com/gustavoakira/devconnect/adapters/inbound/controller/devprofile/DevProfileController.java
@@ -1,11 +1,21 @@
 package br.com.gustavoakira.devconnect.adapters.inbound.controller.devprofile;
 
+import br.com.gustavoakira.devconnect.adapters.inbound.controller.devprofile.dto.DevProfileResponse;
+import br.com.gustavoakira.devconnect.adapters.inbound.controller.devprofile.dto.SaveDevProfileRequest;
+import br.com.gustavoakira.devconnect.application.domain.DevProfile;
+import br.com.gustavoakira.devconnect.application.domain.exceptions.BusinessException;
 import br.com.gustavoakira.devconnect.application.usecases.devprofile.DevProfileUseCases;
+import br.com.gustavoakira.devconnect.application.usecases.devprofile.command.SaveDevProfileCommand;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/dev-profiles/")
@@ -13,4 +23,9 @@ public class DevProfileController {
     @Autowired
     private DevProfileUseCases cases;
 
+    @PostMapping
+    public ResponseEntity<DevProfileResponse> saveDevProfile(@RequestBody @Valid SaveDevProfileRequest request) throws BusinessException {
+        DevProfile profile = cases.saveDevProfileUseCase().execute(request.toCommand());
+        return ResponseEntity.created(URI.create("/v1/dev-profiles/"+profile.getId())).body(DevProfileResponse.fromDomain(profile));
+    }
 }
---
+++ src/main/java/br/com/gustavoakira/devconnect/adapters/inbound/controller/devprofile/dto/DevProfileResponse.java
@@ -0,0 +1,43 @@
+package br.com.gustavoakira.devconnect.adapters.inbound.controller.devprofile.dto;
+
+import br.com.gustavoakira.devconnect.application.domain.DevProfile;
+import lombok.Data;
+
+@Data
+public class DevProfileResponse {
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String bio;
+    private final AddressResponse address;
+    private final String githubLink;
+    private final String linkedinLink;
+
+
+    @Data
+    static class AddressResponse{
+        private final String street;
+        private final String city;
+        private final String state;
+        private final String zipCode;
+        private final String country;
+    }
+
+    public static DevProfileResponse fromDomain(DevProfile profile){
+        return new DevProfileResponse(
+                profile.getId(),
+                profile.getName(),
+                profile.getEmail(),
+                profile.getBio(),
+                new AddressResponse(
+                        profile.getAddress().getStreet(),
+                        profile.getAddress().getCity(),
+                        profile.getAddress().getState(),
+                        profile.getAddress().getZipCode(),
+                        profile.getAddress().getCountry()
+                ),
+                profile.getGithubLink(),
+                profile.getLinkedinLink()
+        );
+    }
+}
