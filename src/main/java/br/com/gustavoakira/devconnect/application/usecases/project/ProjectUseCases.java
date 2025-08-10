8de51212a78fe61e01c3be17db227244928fa119
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-08-10 17:25:12 -0300

    feat: adding delete project endpoint

--- src/main/java/br/com/gustavoakira/devconnect/application/usecases/project/ProjectUseCases.java
+++ src/main/java/br/com/gustavoakira/devconnect/application/usecases/project/ProjectUseCases.java
@@ -7,11 +7,13 @@ public class ProjectUseCases {
     private final SaveProjectUseCase saveProjectUseCase;
     private final FindProjectByIdUseCase findProjectByIdUseCase;
     private final UpdateProjectUseCase updateProjectUseCase;
+    private final DeleteProjectUseCase deleteProjectUseCase;
 
-    public ProjectUseCases(SaveProjectUseCase saveDevProfileUseCase, FindProjectByIdUseCase findProjectByIdUseCase, UpdateProjectUseCase updateProjectUseCase) {
+    public ProjectUseCases(SaveProjectUseCase saveDevProfileUseCase, FindProjectByIdUseCase findProjectByIdUseCase, UpdateProjectUseCase updateProjectUseCase, DeleteProjectUseCase deleteProjectUseCase) {
         this.saveProjectUseCase = saveDevProfileUseCase;
         this.findProjectByIdUseCase = findProjectByIdUseCase;
         this.updateProjectUseCase = updateProjectUseCase;
+        this.deleteProjectUseCase = deleteProjectUseCase;
     }
 
     public FindProjectByIdUseCase getFindProjectByIdUseCase() {
@@ -24,5 +26,9 @@ public class ProjectUseCases {
 
     public SaveProjectUseCase getSaveProjectUseCase() {
         return saveProjectUseCase;
+    }
+
+    public DeleteProjectUseCase getDeleteProjectUseCase() {
+        return deleteProjectUseCase;
     }
 }
