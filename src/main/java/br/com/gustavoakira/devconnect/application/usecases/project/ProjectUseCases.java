9071b4282a180516dd47a8740e33ad6033623a92
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-08-09 23:28:19 -0300

    feat: update project endpoint

--- src/main/java/br/com/gustavoakira/devconnect/application/usecases/project/ProjectUseCases.java
+++ src/main/java/br/com/gustavoakira/devconnect/application/usecases/project/ProjectUseCases.java
@@ -6,14 +6,20 @@ import org.springframework.stereotype.Component;
 public class ProjectUseCases {
     private final SaveProjectUseCase saveProjectUseCase;
     private final FindProjectByIdUseCase findProjectByIdUseCase;
+    private final UpdateProjectUseCase updateProjectUseCase;
 
-    public ProjectUseCases(SaveProjectUseCase saveDevProfileUseCase, FindProjectByIdUseCase findProjectByIdUseCase) {
+    public ProjectUseCases(SaveProjectUseCase saveDevProfileUseCase, FindProjectByIdUseCase findProjectByIdUseCase, UpdateProjectUseCase updateProjectUseCase) {
         this.saveProjectUseCase = saveDevProfileUseCase;
         this.findProjectByIdUseCase = findProjectByIdUseCase;
+        this.updateProjectUseCase = updateProjectUseCase;
     }
 
     public FindProjectByIdUseCase getFindProjectByIdUseCase() {
         return findProjectByIdUseCase;
+    }
+
+    public UpdateProjectUseCase getUpdateProjectUseCase() {
+        return updateProjectUseCase;
     }
 
     public SaveProjectUseCase getSaveProjectUseCase() {
--- src/test/java/br/com/gustavoakira/devconnect/adapters/inbound/controller/project/ProjectControllerTest.java
+++ src/test/java/br/com/gustavoakira/devconnect/adapters/inbound/controller/project/ProjectControllerTest.java
@@ -3,12 +3,14 @@ package br.com.gustavoakira.devconnect.adapters.inbound.controller.project;
 import br.com.gustavoakira.devconnect.adapters.config.JwtProvider;
 import br.com.gustavoakira.devconnect.adapters.inbound.controller.devprofile.DevProfileController;
 import br.com.gustavoakira.devconnect.adapters.inbound.controller.project.dto.CreateProjectRequest;
+import br.com.gustavoakira.devconnect.adapters.inbound.controller.project.dto.UpdateProjectRequest;
 import br.com.gustavoakira.devconnect.adapters.outbound.exceptions.EntityNotFoundException;
 import br.com.gustavoakira.devconnect.application.domain.Project;
 import br.com.gustavoakira.devconnect.application.domain.exceptions.BusinessException;
 import br.com.gustavoakira.devconnect.application.usecases.project.FindProjectByIdUseCase;
 import br.com.gustavoakira.devconnect.application.usecases.project.ProjectUseCases;
 import br.com.gustavoakira.devconnect.application.usecases.project.SaveProjectUseCase;
+import br.com.gustavoakira.devconnect.application.usecases.project.UpdateProjectUseCase;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
@@ -35,8 +37,7 @@ import java.util.List;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -58,6 +59,9 @@ class ProjectControllerTest {
     @Mock
     private FindProjectByIdUseCase findProjectByIdUseCase;
 
+    @Mock
+    private UpdateProjectUseCase updateProjectUseCase;
+
     @MockitoBean
     private ProjectUseCases useCases;
 
@@ -67,6 +71,7 @@ class ProjectControllerTest {
         SecurityContextHolder.getContext().setAuthentication(auth);
         Mockito.when(useCases.getSaveProjectUseCase()).thenReturn(saveProjectUseCase);
         Mockito.when(useCases.getFindProjectByIdUseCase()).thenReturn(findProjectByIdUseCase);
+        Mockito.when(useCases.getUpdateProjectUseCase()).thenReturn(updateProjectUseCase);
     }
 
     @Nested
@@ -105,6 +110,35 @@ class ProjectControllerTest {
             Mockito.when(findProjectByIdUseCase.execute(1L)).thenReturn(new Project(1L,projectName,projectDescription,projectUrl,1L));
             mockMvc.perform(get("/v1/projects/1"))
                     .andExpect(status().is2xxSuccessful())
+                    .andExpect(jsonPath("$.id", is(1)))
+                    .andExpect(jsonPath("$.name", is(projectName)))
+                    .andExpect(jsonPath("$.description", is(projectDescription)))
+                    .andExpect(jsonPath("$.devProfileId", is(1)))
+                    .andExpect(jsonPath("$.repoUrl", is(projectUrl)));
+        }
+    }
+
+    @Nested
+    class UpdateProject{
+        @Test
+        void shouldSaveProjectAndReturn200HttpStatusCode() throws Exception {
+            final String projectName = "akira";
+            final String projectDescription = "description";
+            final String projectUrl = "https://github.com";
+
+            final UpdateProjectRequest request = new UpdateProjectRequest(
+                    1L,
+                    projectName,
+                    projectDescription,
+                    projectUrl
+            );
+            Mockito.when(updateProjectUseCase.execute(request.toDomain(1L))).thenReturn(new Project(1L,projectName,projectDescription,projectUrl,1L));
+
+
+            mockMvc.perform(put("/v1/projects")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isOk())
                     .andExpect(jsonPath("$.id", is(1)))
                     .andExpect(jsonPath("$.name", is(projectName)))
                     .andExpect(jsonPath("$.description", is(projectDescription)))
