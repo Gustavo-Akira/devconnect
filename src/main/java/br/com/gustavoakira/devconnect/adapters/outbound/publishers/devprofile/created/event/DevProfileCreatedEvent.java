873ad33fc1fd3be0eb59907f37ead4b705ea0dd6
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-10-24 23:43:17 -0300

    feat: adding profile info relevant to relation in the event

--- src/main/java/br/com/gustavoakira/devconnect/adapters/outbound/publishers/devprofile/created/event/DevProfileCreatedEvent.java
+++ src/main/java/br/com/gustavoakira/devconnect/adapters/outbound/publishers/devprofile/created/event/DevProfileCreatedEvent.java
@@ -4,13 +4,21 @@ import br.com.gustavoakira.devconnect.application.domain.DevProfile;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor
 @Data
 public class DevProfileCreatedEvent {
     private Long id;
     private String name;
+    private String city;
+    private List<String> stack = new ArrayList<>();
+
     public DevProfileCreatedEvent(DevProfile profile){
         this.id = profile.getId();
         this.name = profile.getName();
+        this.city = profile.getAddress().getCity();
+        this.stack = profile.getStack();
     }
 }
